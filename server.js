914cea760d7e8cf3c030536fd31bfb3f9d8bd848
Ryan Chenkie <ryanchenkie@gmail.com>
2017-03-23 23:23:41 -0500

    prep finished

--- server.js
+++ server.js
@@ -1,22 +1,11 @@
 'use strict';
 
 const Hapi = require('hapi');
-const Inert = require('inert');
-const Geolocate = require('hapi-geo-locate');
-const path = require('path');
 
 // Starting a Hapi server simply requires
 // an instance of Hapi.Server. We leave it
 // bare or we can add some configuration
-const server = new Hapi.Server({
-  connections: {
-    routes: {
-      files: {
-        relativeTo: path.join(__dirname, 'public')
-      }
-    }
-  }
-});
+const server = new Hapi.Server();
 
 // We need to specify a connection, which
 // we can default to the port specified in
@@ -32,27 +21,11 @@ server.connection({
   }
 });
 
-// The simple way to register a plugin
-// is to pass the module and a callback
-server.register(Inert, () => {});
-
-// To configure the plugin, we can pass
-// an object which has an options key
-server.register({
-  register: Geolocate,
-  options: {
-    enabledByDefault: true
-  }
-}, err => {
-  if (err) console.log(err);
-});
-
 // We're defining our route configuration in separate files
 // and creating new routes with that configuration here
 server.route(require('./api/instructors/routes/get_instructors'));
 server.route(require('./api/instructors/routes/get_instructor'));
 server.route(require('./api/instructors/routes/post_instructor'));
-server.route(require('./api/images/routes/get_image'));
 
 // Starting the server is as simple as calling
 // server.start. We can throw an error if something
