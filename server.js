3ae16bbe1daf694360f2ba7b7b28b209ab52e075
Ryan Chenkie <ryanchenkie@gmail.com>
2017-03-24 00:01:59 -0500

    prep start

--- server.js
+++ server.js
@@ -2,37 +2,10 @@
 
 const Hapi = require('hapi');
 
-// Starting a Hapi server simply requires
-// an instance of Hapi.Server. We leave it
-// bare or we can add some configuration
-const server = new Hapi.Server();
+// Create a Hapi server instance
 
-// We need to specify a connection, which
-// we can default to the port specified in
-// an ENV variable, or 3001 if none is set.
-// We also need to configure CORS for requests
-// coming from a single page app
-server.connection({
-  port: process.env.port || 3001,
-  routes: {
-    cors: {
-      origin: ['*']
-    }
-  }
-});
+// Tell the server which port to listen on
 
-server.route({
-  method: 'GET',
-  path: '/hello',
-  handler: (request, reply) => {
-    reply({ message: 'Hey there' });
-  }
-});
+// Create a hello world route
 
-// Starting the server is as simple as calling
-// server.start. We can throw an error if something
-// goes wrong
-server.start(err => {
-  if (err) throw err;
-  console.log(`Server listening at ${server.info.uri}`);
-});
+// Start the server and log out the URI
